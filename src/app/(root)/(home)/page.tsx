b12d77f1c5fd92b48c004c4698bcb8e63bbf1855
Azizcan Balcı <azizcanbalci07@gmail.com>
2025-04-23 23:21:43 +0300

    Files of the sections on the home page have been created.Added some constant values ​​to be used in the application

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -9,3 +9,4 @@ Home Page
 </div>
   );
 }
+ 
---
+++ src/app/(root)/recordings/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ function RecordingsPage() {
+  return (
+    <div>
+      RecordingsPage
+    </div>
+  )
+}
+export default RecordingsPage
